31ca1d75157e33d411e2353d8b07f96ed72dd492
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-04 13:19:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,9 @@ client.on('message', message => {
       }
 });
 
-
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","[ Rocketer User ]"));
+    });
 
 
 client.on("message", message => {
