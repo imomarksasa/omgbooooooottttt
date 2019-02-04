20340348c66970fbafe3be1a53bc49454c3ff6d2
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-04 13:14:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,128 +14,23 @@ client.on('message', message => {
 
 
 
-client.on('message', async msg => {
-    var prefix = ".";
-    var user = msg.author;
-            var a = msg.guild.roles.find("name", 'Test');
-        if(!a){
-        a = await msg.guild.createRole({
-          name: "Test",
-          color: "#ffffff",
-          permissions:[]
-        })
-       
-        }
-        var m = msg.guild.roles.find("name", 'Minecraft');
-    if(!m){
-        m =  await msg.guild.createRole({
-          name: "Minecraft",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
-        var f = msg.guild.roles.find("name", 'Fortnite');
-        if(!f){
-        f =  await msg.guild.createRole({
-          name: "Fortnite",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
-var m = msg.guild.roles.find("name", 'Minecraft');
-    if(!m){
-        m =  await msg.guild.createRole({
-          name: "Minecraft",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
-        var f = msg.guild.roles.find("name", 'Cs GO');
-        if(!f){
-        f =  await msg.guild.createRole({
-          name: "Cs GO",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
-        var b = msg.guild.roles.find("name", 'Creative Destruction');
-        if(!b){
-        b =  await msg.guild.createRole({
-          name: "Creative Destruction",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
-        var black = msg.guild.roles.find("name", 'Blacksquad');
-    if(!black){
-        black =  await msg.guild.createRole({
-          name: "Blacksquad",
-          color: "#ffffff",
-          permissions:[]
-        })
-        }
- 
-        if (msg.content.startsWith(prefix +'addmerole')) {
- 
-        if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-msg.channel.send(`يرحي اختيار رتبة اللعبة الذي تريدها \n1- لعبة اقاريو ⚽ \n2- لعبة ماين كرافت 👶 \n3- لعبة فورت نايت 👊 \n4- لعبة براوهلا  👌 \n5- لعبة بلاك سكواد 🍸\n6- الغاء ❌ \n7- **لديك60 ثانية للاختيار **\n<@${msg.author.id}>`).then(res => {    
-     res.react('⚽').then(r=>{    
-     res.react('👶').then(r=>{
-     res.react('👊').then(r=>{
-     res.react('👌').then(r=>{
-     res.react('🍸').then(r=>{
-     res.react('❌').then(r=>{
- 
-    let aaa = (reaction, user) => reaction.emoji.name === '⚽' && user.id === msg.author.id;    
-    let mmm = (reaction, user) => reaction.emoji.name === '👶' && user.id === msg.author.id;
-    let fff = (reaction, user) => reaction.emoji.name === '👊' && user.id === msg.author.id;
-    let bbb = (reaction, user) => reaction.emoji.name === '👌' && user.id === msg.author.id;
-    let bbbb = (reaction, user) => reaction.emoji.name === '🍸' && user.id === msg.author.id;
-    let ccc = (reaction, user) => reaction.emoji.name === '❌' && user.id === msg.author.id;
- 
-    let aa = res.createReactionCollector(aaa, { maxMatches:1 , time: 20000 , });
-    let mm = res.createReactionCollector(mmm, { maxMatches:1 , time: 20000 , });
-    let ff = res.createReactionCollector(fff, { maxMatches:1 , time: 20000 , });
-    let bb = res.createReactionCollector(bbb, { maxMatches:1 , time: 20000 , });
-    let bl = res.createReactionCollector(bbbb,{ maxMatches:1 , time: 20000 , });
-    let cc = res.createReactionCollector(ccc, { maxMatches:1 , time: 20000 , });
- 
-aa.on("collect", r => {
-    msg.guild.member(user.id).addRole(a);
-    msg.channel.send('`تم اعطائك رتبة للعبة Agar`');
-    msg.delete();
-    })
-mm.on("collect", r => {
-    msg.guild.member(user.id).addRole(m);
-    msg.channel.send('`تم تفعيل حسابك بنجاح :heavy_check_mark:`');
-    msg.delete();
+client.on("message", message => {
+    if(message.content.startsWith("verify")) {
+      let num = Math.floor((Math.random() * 4783) + 10);
+    
+      message.channel.send(`يرجاء كتابة الرقم التالي: **${num}**`).then(m => {
+        message.channel.awaitMessages(res => res.content == `${num}`, {
+          max: 1,
+          time: 60000,
+          errors: ['time'],
+        }).then(collected => {
+          message.delete();
+          m.delete();
+          message.member.addRole(message.guild.roles.find(c => c.name == "[ Rocketer User ]"));
+        }).catch(() => {
+          m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
+});
 })
-ff.on("collect", r => {
-    msg.guild.member(user.id).addRole(f);
-    msg.channel.send('`تم اعطائك رتبة للعبة Fortnite `');
-    msg.delete();
+}
 })
-bb.on("collect", r => {
-    msg.guild.member(user.id).addRole(b);
-    msg.channel.send('`تم اعطائك رتبة للعبة Brawlhalla `');
-    msg.delete();
-})
-bl.on("collect", r => {
-    msg.guild.member(user.id).addRole(black);
-    msg.channel.send('`تم اعطائك رتبة للعبة Blacksquad `');
-    msg.delete();
-})
-cc.on("collect", r => {
-    msg.delete();
-})
-     })
-     })
-     })
-     })
-     })
-     })
-     })
-     }
-     });
-
 client.login(process.env.BOT_TOKEN);
